097b609d3696226ae13acc32ef03c6476b4d0632
yangqiwang <simple11@vip.qq.com>
2017-02-07 11:55:27 +0800

    RxJs学习 例子

---
+++ README.md
@@ -0,0 +1,36 @@
+﻿# rxjs-example
+
+通过构建一个简单的 github 应用，学习 RxJS 的快速入门教程。
+
+[ReactiveX/RxJava文档中文版](https://mcxiaoke.gitbooks.io/rxdocs/content/)
+
+## 开始前的准备
+
+- 使用 ES6 语法
+- 通过 webpack/webpack-dev-server 进行编译
+- 通过[探索 RxJS - Core Concept](https://github.com/ecmadao/Coding-Guide/blob/master/Notes/RxJS/%E6%8E%A2%E7%B4%A2RxJS-CoreConcept.md)学习 RxJS 的核心概念
+
+## 本地跑起来
+
+```bash
+$ git clone https://github.com/ecmadao/rxjs-example.git
+$ cd rxjs-example
+$ npm i
+$ npm run dev-server
+```
+
+然后打开`http://localhost:8080/webpack-dev-server`即可
+
+## 展示
+
+- 通过`RxJS`，在输入过程中实时进行异步搜索：
+
+![search repos](./readme/search_repos.png)
+
+- `hover`到 avator 上之后异步获取用户信息
+
+![fetch user info](./readme/fetch_info.png)
+
+## 教程
+
+> 猛戳这里看教程：[ReactiveX/RxJava文档中文版](https://mcxiaoke.gitbooks.io/rxdocs/content/)
---
+++ src/js/index.js
@@ -0,0 +1,145 @@
+import Rx from 'rx';
+import {
+  getRepos,
+  getUser,
+  formatRepoSizeAndUnit
+} from './helper';
+import '../css/base.css';
+
+const reposTemplate = (repos) => {
+  const {owner} = repos;
+  return `<div class="repos_item">
+    <div class="repos_item_wrapper">
+      <div class="repos_user_container">
+        <div class="user_header_container">
+          <img data-api="${owner.url}" class="user_header" src="${owner.avatar_url}"/>
+          <div class="user_infos_container">
+            <div class="user_infos_wrapper">
+              <div class="info_loading">
+                <i aria-hidden="true" class="fa fa-spinner fa-spin"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="user_info">
+          <a class="user_link" target="_blank" href="${owner.html_url}">
+            ${owner.login}
+          </a> /
+          <a class="repos_link" target="_blank" href="${repos.html_url}">
+            ${repos.name}
+          </a>
+        </div>
+      </div>
+      <div class="repos_info_container">
+        <div class="repos_desc">
+          ${repos.description}
+        </div>
+        <div class="repos_info">
+          <em class="repos_language">${repos.language || ''}</em>
+          <i aria-hidden="true" class="fa fa-star"></i>&nbsp;${repos.stargazers_count}&nbsp;&nbsp;
+          <i aria-hidden="true" class="fa fa-eye"></i>&nbsp;${repos.watchers_count}&nbsp;&nbsp;
+          <i aria-hidden="true" class="fa fa-code-fork"></i>&nbsp;${repos.forks_count}&nbsp;&nbsp;
+          <i aria-hidden="true" class="fa fa-file-archive-o"></i>&nbsp;${formatRepoSizeAndUnit(repos.size).join(' ')}
+        </div>
+      </div>
+    </div>
+  </div>`;
+};
+
+const showUserInfo = ($dom, data) => {
+  $dom.html(userTemplate(data));
+};
+
+const userTemplate = (user) => {
+  return `<div class="infos_container">
+    <div class="info_container">
+      <div class="info_icon">
+        <i aria-hidden="true" class="fa fa-user-circle"></i>
+      </div>&nbsp;&nbsp;${user.name || user.login}
+    </div>
+    ${user.location ? `<div class="info_container">
+      <div class="info_icon">
+        <i aria-hidden="true" class="fa fa-map-marker"></i>
+      </div>&nbsp;&nbsp;${user.location}
+    </div>` : ''}
+    ${user.email ? `<a class="info_container" href="mailto:${user.email}">
+      <div class="info_icon">
+        <i aria-hidden="true" class="fa fa-envelope-o"></i>
+      </div>&nbsp;&nbsp;${user.email}
+    </a>` : ''}
+    ${user.company ? `<div class="info_container">
+      <div class="info_icon">
+        <i aria-hidden="true" class="fa fa-users"></i>
+      </div>&nbsp;&nbsp;${user.company}
+    </div>` : ''}
+    ${user.blog ? `<a class="info_container" target="_blank" href="${user.blog}">
+      <div class="info_icon">
+        <i aria-hidden="true" class="fa fa-chrome"></i>
+      </div>&nbsp;&nbsp;${user.blog}
+    </a>` : ''}
+    ${user.bio ? `<div class="info_container info_bio">
+      <div class="info_icon">
+        <i aria-hidden="true" class="fa fa-quote-left"></i>
+      </div>&nbsp;&nbsp;${user.bio}
+    </div>` : ''}
+    <div class="info_container info_social">
+      <i aria-hidden="true" class="fa fa-cube"></i>&nbsp;${user.public_repos}&nbsp;&nbsp;
+      <i aria-hidden="true" class="fa fa-user-plus"></i>&nbsp;${user.followers}&nbsp;&nbsp;
+      <i aria-hidden="true" class="fa fa-calendar-check-o"></i>&nbsp;${user.updated_at.split('T')[0]}
+    </div>
+  </div>`;
+};
+
+const userInfoSteam = ($repos) => {
+  const $avator = $repos.find('.user_header');
+  const avatorMouseoverObservable = Rx.Observable.fromEvent($avator, 'mouseover')
+    .debounce(500)
+    .takeWhile((e) => {
+      const $infosWrapper = $(e.target).parent().find('.user_infos_wrapper');
+      return $infosWrapper.find('.infos_container').length === 0;
+    })
+    .map((e) => {
+      const $infosWrapper = $(e.target).parent().find('.user_infos_wrapper');
+      return {
+        conatiner: $infosWrapper,
+        url: $(e.target).attr('data-api')
+      }
+    })
+    .filter((data) => !!data.url)
+    .flatMapLatest(getUser)
+    .do((result) => {
+      const {data, conatiner} = result;
+      showUserInfo(conatiner, data);
+    });
+
+  return avatorMouseoverObservable;
+};
+
+$(() => {
+  const $conatiner = $('.content_container');
+  const $input = $('.search');
+  const observable = Rx.Observable.fromEvent($input, 'keyup')
+    .debounce(400)
+    .map(() => $input.val().trim())
+    .filter((text) => !!text)
+    .distinctUntilChanged()
+    .flatMapLatest(getRepos)
+    .do((results) => $conatiner.html(''))
+    .flatMap((results) => Rx.Observable.from(results))
+    .map((repos) => $(reposTemplate(repos)))
+    .do(($repos) => {
+      $conatiner.append($repos);
+      // initialUserInfoSteam($repos);
+    })
+    .flatMap(($repos) => {
+      return userInfoSteam($repos);
+    });
+
+  observable.subscribe(() => {
+    console.log('success');
+  }, (err) => {
+    console.log(err);
+  }, () => {
+    console.log('completed');
+  });
+});
